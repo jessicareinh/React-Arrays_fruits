8f1a2c9e0a5d549eb2420905578ee85eac1b776f
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-01-09 12:46:34 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import Card from "./components/Card";
+import React from "react";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: "122",
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+
+    {
+      id: "123",
+      name: "🍇 Grape",
+      color: "red",
+    },
+
+    {
+      id: "124",
+      name: "🍉 Watermelon",
+      color: "green",
+    },
+
+    {
+      id: "125",
+      name: "🍓 Strawberry",
+      color: "red",
+    },
+
+    {
+      id: "126",
+      name: "🥝 Kiwi",
+      color: "brown",
+    }
+  ];
 
   return (
-    <div className="app">
-      <Card name="🍌 banana" />
-    </div>
+
+    <ul className="app">
+      {fruits.map((fruit) => {
+        return (
+          <li key={fruit.id}>
+            <Card
+              name={fruit.name}
+              color={fruit.color}
+            />
+          </li>
+        );
+      }
+      )}
+    </ul>
   );
 }
+
+
